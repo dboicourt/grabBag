128148cb56c80bd55171078d5a2eef1ae5c085c3
dboicourt <dboicourt@gmail.com>
2022-02-01 16:40:28 -0800

    Added product name overlay

--- src/Components/ImageGrid.js
+++ src/Components/ImageGrid.js
@@ -5,7 +5,7 @@ import Product from "./Product";
 class ImageGrid extends React.Component {
   render() {
     return (
-      <ImageList sx={{ width: 700, height: 600 }} cols={3} rowHeight={300}>
+      <ImageList sx={{ width: 1200, height: 600 }} cols={3} rowHeight={300} gap={4}>
         {this.props.items.map((item) => (
           <Product product={item}></Product>
         ))}
